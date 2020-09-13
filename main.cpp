f7371945eb57aa27588a10f5b075697254063fd6
cramik <5573629+cramik@users.noreply.github.com>
2020-09-13 16:46:15 -0700

    Attributing WriteCallback function

--- main.cpp
+++ main.cpp
@@ -6,9 +6,11 @@
 using namespace std;
 using json = nlohmann::json;
 
+
+// Function stolen for CURL_WriteData so we can parse the response
 size_t WriteCallback(char *contents, size_t size, size_t nmemb, void *userp)
 {
-    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    ((string*)userp)->append((char*)contents, size * nmemb);
     return size * nmemb;
 }
 
